646bc2581323618f3d8e79e65484f8b88cbe60df
Jonathan Prieto <jonathan.prieto@me.com>
2020-10-14 10:22:50 -0500

    First Push Code Publisher

--- src/main/java/lab9/Publisher.java
+++ src/main/java/lab9/Publisher.java
@@ -1,4 +1,12 @@
 package lab9;
 
 public class Publisher {
+
+    String mensaje;
+    String canal;
+
+    public void publish(String mensaje, String canal) {
+        MessageBroker.enviarMessage(mensaje, canal);
+    }
+
 }
