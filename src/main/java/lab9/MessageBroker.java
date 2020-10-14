a6b2afdf5960bab9ebf742e289fe9349e2c414a7
mauriciopinto <mauricio.pinto@utec.edu.pe>
2020-10-14 10:39:53 -0500

    broker singleton

--- src/main/java/lab9/MessageBroker.java
+++ src/main/java/lab9/MessageBroker.java
@@ -5,8 +5,9 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class MessageBroker {
+    private static MessageBroker instance;
     private HashMap<String, List<Subscriber>> subscribers = new HashMap<String, List<Subscriber>>();
-    private int semaphore = 0;
+    private static int semaphore = 0;
     private MessageBroker (){};
     public void registerSubscriber (Subscriber s, String channel) {
         subscribers.get(channel).add(s);
@@ -18,9 +19,15 @@ public class MessageBroker {
         subscribers.get(channel).forEach(displayMessage );
     }
 
-
+    private synchronized static int increaseSemaphore () {
+        int prev = semaphore;
+        semaphore++;
+        return prev;
+    }
 
     public static MessageBroker getInstance() {
-
+        if (increaseSemaphore () == 0)
+            instance = new MessageBroker();
+        return instance;
     }
 }
