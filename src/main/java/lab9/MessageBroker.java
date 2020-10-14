c3e4ba6fbb51407a57671117986e8dc1fe648f9c
mauriciopinto <mauricio.pinto@utec.edu.pe>
2020-10-14 10:34:17 -0500

    broker

--- src/main/java/lab9/MessageBroker.java
+++ src/main/java/lab9/MessageBroker.java
@@ -1,4 +1,26 @@
 package lab9;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MessageBroker {
+    private HashMap<String, List<Subscriber>> subscribers = new HashMap<String, List<Subscriber>>();
+    private int semaphore = 0;
+    private MessageBroker (){};
+    public void registerSubscriber (Subscriber s, String channel) {
+        subscribers.get(channel).add(s);
+    }
+    public void removeSubscriber (Subscriber s, String channel) {
+        subscribers.get(channel).remove(s);
+    }
+    public void notifySubscriber (String message, String channel) {
+        subscribers.get(channel).forEach(displayMessage );
+    }
+
+
+
+    public static MessageBroker getInstance() {
+
+    }
 }
