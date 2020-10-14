dea10f3428744eb744c2c340a64706c100b5fb3a
FabrizioVx <fabrizio.vasquez@utec.edu.pe>
2020-10-14 10:45:24 -0500

    subscribe

--- src/main/java/lab9/Subscriber.java
+++ src/main/java/lab9/Subscriber.java
@@ -1,4 +1,10 @@
 package lab9;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Subscriber {
+    public void subscribe(String channel){
+        suscribers.getInstance().registerSubcriber(this,channel);
+    }
 }
