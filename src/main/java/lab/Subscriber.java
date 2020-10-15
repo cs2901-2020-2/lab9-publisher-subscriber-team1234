7dc7e1f9aa7ea2dba441ebb0d194d877068469d9
v1c7or20 <victor.pena@utec.edu.pe>
2020-10-15 08:23:19 -0500

    up

--- src/main/java/lab/Subscriber.java
+++ src/main/java/lab/Subscriber.java
@@ -1,15 +1,19 @@
 package lab;
 
+import java.util.logging.Logger;
+
 public class Subscriber {
     private String name;
     private String data;
+    static final Logger logger = Logger.getLogger(Subscriber.class.getName());
 
     public Subscriber(String nameTo){
         name = nameTo;
     }
 
     public void displayMessage(){
-
+        String log = name + " message is: " + data;
+        logger.info(log);
     }
 
     public void subscribe(String channel){
